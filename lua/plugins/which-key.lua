2ef7861385eed65d50829dfbaa80f7e2efcb447f
StoicCat <josepgultom25@gmail.com>
2026-04-14 19:54:26 +0700

    Fix which key to show correct leader commands

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,6 +2,18 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   config = function()
-    require("which-key").setup({})
+    local wk = require("which-key")
+
+    wk.setup({})
+
+    wk.add({
+      { "<leader>b", group = "Buffers" },
+      { "<leader>c", group = "Code" },
+      { "<leader>e", group = "Explorer" },
+      { "<leader>f", group = "Files" },
+      { "<leader>h", group = "Harpoon" },
+      { "<leader>r", group = "Run" },
+      { "<leader>y", group = "Yank" },
+    })
   end,
 }
